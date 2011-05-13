46ee5ad35818cd7e25c5d4c949c1832d9a180c0a
Yurius Radomskyi <yurius.r@gmail.com>
2011-05-13 14:39:20 +0300

    we need .gitignore file

---
+++ java/decaf/DecafScanner.java
@@ -0,0 +1,436 @@
+// $ANTLR 2.7.7 (2006-11-01): "Lexer.g" -> "DecafScanner.java"$
+package decaf;
+import java.io.InputStream;
+import antlr.TokenStreamException;
+import antlr.TokenStreamIOException;
+import antlr.TokenStreamRecognitionException;
+import antlr.CharStreamException;
+import antlr.CharStreamIOException;
+import antlr.ANTLRException;
+import java.io.Reader;
+import java.util.Hashtable;
+import antlr.CharScanner;
+import antlr.InputBuffer;
+import antlr.ByteBuffer;
+import antlr.CharBuffer;
+import antlr.Token;
+import antlr.CommonToken;
+import antlr.RecognitionException;
+import antlr.NoViableAltForCharException;
+import antlr.MismatchedCharException;
+import antlr.TokenStream;
+import antlr.ANTLRHashString;
+import antlr.LexerSharedInputState;
+import antlr.collections.impl.BitSet;
+import antlr.SemanticException;
+
+public class DecafScanner extends antlr.CharScanner implements DecafScannerTokenTypes, TokenStream
+ {
+public DecafScanner(InputStream in) {
+	this(new ByteBuffer(in));
+}
+public DecafScanner(Reader in) {
+	this(new CharBuffer(in));
+}
+public DecafScanner(InputBuffer ib) {
+	this(new LexerSharedInputState(ib));
+}
+public DecafScanner(LexerSharedInputState state) {
+	super(state);
+	caseSensitiveLiterals = true;
+	setCaseSensitive(true);
+	literals = new Hashtable();
+	literals.put(new ANTLRHashString("class", this), new Integer(4));
+}
+
+public Token nextToken() throws TokenStreamException {
+	Token theRetToken=null;
+tryAgain:
+	for (;;) {
+		Token _token = null;
+		int _ttype = Token.INVALID_TYPE;
+		resetText();
+		try {   // for char stream error handling
+			try {   // for lexical error handling
+				switch ( LA(1)) {
+				case '{':
+				{
+					mLCURLY(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case '}':
+				{
+					mRCURLY(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case 'A':  case 'B':  case 'C':  case 'D':
+				case 'E':  case 'F':  case 'G':  case 'H':
+				case 'I':  case 'J':  case 'K':  case 'L':
+				case 'M':  case 'N':  case 'O':  case 'P':
+				case 'Q':  case 'R':  case 'S':  case 'T':
+				case 'U':  case 'V':  case 'W':  case 'X':
+				case 'Y':  case 'Z':  case 'a':  case 'b':
+				case 'c':  case 'd':  case 'e':  case 'f':
+				case 'g':  case 'h':  case 'i':  case 'j':
+				case 'k':  case 'l':  case 'm':  case 'n':
+				case 'o':  case 'p':  case 'q':  case 'r':
+				case 's':  case 't':  case 'u':  case 'v':
+				case 'w':  case 'x':  case 'y':  case 'z':
+				{
+					mID(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case '\n':  case ' ':
+				{
+					mWS_(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case '/':
+				{
+					mSL_COMMENT(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case '\'':
+				{
+					mCHAR(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				case '"':
+				{
+					mSTRING(true);
+					theRetToken=_returnToken;
+					break;
+				}
+				default:
+				{
+					if (LA(1)==EOF_CHAR) {uponEOF(); _returnToken = makeToken(Token.EOF_TYPE);}
+				else {throw new NoViableAltForCharException((char)LA(1), getFilename(), getLine(), getColumn());}
+				}
+				}
+				if ( _returnToken==null ) continue tryAgain; // found SKIP token
+				_ttype = _returnToken.getType();
+				_ttype = testLiteralsTable(_ttype);
+				_returnToken.setType(_ttype);
+				return _returnToken;
+			}
+			catch (RecognitionException e) {
+				throw new TokenStreamRecognitionException(e);
+			}
+		}
+		catch (CharStreamException cse) {
+			if ( cse instanceof CharStreamIOException ) {
+				throw new TokenStreamIOException(((CharStreamIOException)cse).io);
+			}
+			else {
+				throw new TokenStreamException(cse.getMessage());
+			}
+		}
+	}
+}
+
+	public final void mLCURLY(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = LCURLY;
+		int _saveIndex;
+		
+		match("{");
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mRCURLY(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = RCURLY;
+		int _saveIndex;
+		
+		match("}");
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mID(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = ID;
+		int _saveIndex;
+		
+		{
+		int _cnt5=0;
+		_loop5:
+		do {
+			switch ( LA(1)) {
+			case 'a':  case 'b':  case 'c':  case 'd':
+			case 'e':  case 'f':  case 'g':  case 'h':
+			case 'i':  case 'j':  case 'k':  case 'l':
+			case 'm':  case 'n':  case 'o':  case 'p':
+			case 'q':  case 'r':  case 's':  case 't':
+			case 'u':  case 'v':  case 'w':  case 'x':
+			case 'y':  case 'z':
+			{
+				matchRange('a','z');
+				break;
+			}
+			case 'A':  case 'B':  case 'C':  case 'D':
+			case 'E':  case 'F':  case 'G':  case 'H':
+			case 'I':  case 'J':  case 'K':  case 'L':
+			case 'M':  case 'N':  case 'O':  case 'P':
+			case 'Q':  case 'R':  case 'S':  case 'T':
+			case 'U':  case 'V':  case 'W':  case 'X':
+			case 'Y':  case 'Z':
+			{
+				matchRange('A','Z');
+				break;
+			}
+			default:
+			{
+				if ( _cnt5>=1 ) { break _loop5; } else {throw new NoViableAltForCharException((char)LA(1), getFilename(), getLine(), getColumn());}
+			}
+			}
+			_cnt5++;
+		} while (true);
+		}
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mWS_(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = WS_;
+		int _saveIndex;
+		
+		{
+		switch ( LA(1)) {
+		case ' ':
+		{
+			match(' ');
+			break;
+		}
+		case '\n':
+		{
+			match('\n');
+			newline();
+			break;
+		}
+		default:
+		{
+			throw new NoViableAltForCharException((char)LA(1), getFilename(), getLine(), getColumn());
+		}
+		}
+		}
+		_ttype = Token.SKIP;
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mSL_COMMENT(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = SL_COMMENT;
+		int _saveIndex;
+		
+		match("//");
+		{
+		_loop10:
+		do {
+			if ((_tokenSet_0.member(LA(1)))) {
+				matchNot('\n');
+			}
+			else {
+				break _loop10;
+			}
+			
+		} while (true);
+		}
+		match('\n');
+		_ttype = Token.SKIP; newline ();
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mCHAR(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = CHAR;
+		int _saveIndex;
+		
+		match('\'');
+		{
+		switch ( LA(1)) {
+		case '\\':
+		{
+			mESC(false);
+			break;
+		}
+		case '\u0000':  case '\u0001':  case '\u0002':  case '\u0003':
+		case '\u0004':  case '\u0005':  case '\u0006':  case '\u0007':
+		case '\u0008':  case '\t':  case '\n':  case '\u000b':
+		case '\u000c':  case '\r':  case '\u000e':  case '\u000f':
+		case '\u0010':  case '\u0011':  case '\u0012':  case '\u0013':
+		case '\u0014':  case '\u0015':  case '\u0016':  case '\u0017':
+		case '\u0018':  case '\u0019':  case '\u001a':  case '\u001b':
+		case '\u001c':  case '\u001d':  case '\u001e':  case '\u001f':
+		case ' ':  case '!':  case '"':  case '#':
+		case '$':  case '%':  case '&':  case '(':
+		case ')':  case '*':  case '+':  case ',':
+		case '-':  case '.':  case '/':  case '0':
+		case '1':  case '2':  case '3':  case '4':
+		case '5':  case '6':  case '7':  case '8':
+		case '9':  case ':':  case ';':  case '<':
+		case '=':  case '>':  case '?':  case '@':
+		case 'A':  case 'B':  case 'C':  case 'D':
+		case 'E':  case 'F':  case 'G':  case 'H':
+		case 'I':  case 'J':  case 'K':  case 'L':
+		case 'M':  case 'N':  case 'O':  case 'P':
+		case 'Q':  case 'R':  case 'S':  case 'T':
+		case 'U':  case 'V':  case 'W':  case 'X':
+		case 'Y':  case 'Z':  case '[':  case ']':
+		case '^':  case '_':  case '`':  case 'a':
+		case 'b':  case 'c':  case 'd':  case 'e':
+		case 'f':  case 'g':  case 'h':  case 'i':
+		case 'j':  case 'k':  case 'l':  case 'm':
+		case 'n':  case 'o':  case 'p':  case 'q':
+		case 'r':  case 's':  case 't':  case 'u':
+		case 'v':  case 'w':  case 'x':  case 'y':
+		case 'z':  case '{':  case '|':  case '}':
+		case '~':  case '\u007f':
+		{
+			matchNot('\'');
+			break;
+		}
+		default:
+		{
+			throw new NoViableAltForCharException((char)LA(1), getFilename(), getLine(), getColumn());
+		}
+		}
+		}
+		match('\'');
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	protected final void mESC(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = ESC;
+		int _saveIndex;
+		
+		match('\\');
+		{
+		switch ( LA(1)) {
+		case 'n':
+		{
+			match('n');
+			break;
+		}
+		case '"':
+		{
+			match('"');
+			break;
+		}
+		default:
+		{
+			throw new NoViableAltForCharException((char)LA(1), getFilename(), getLine(), getColumn());
+		}
+		}
+		}
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	public final void mSTRING(boolean _createToken) throws RecognitionException, CharStreamException, TokenStreamException {
+		int _ttype; Token _token=null; int _begin=text.length();
+		_ttype = STRING;
+		int _saveIndex;
+		
+		match('"');
+		{
+		_loop15:
+		do {
+			switch ( LA(1)) {
+			case '\\':
+			{
+				mESC(false);
+				break;
+			}
+			case '\u0000':  case '\u0001':  case '\u0002':  case '\u0003':
+			case '\u0004':  case '\u0005':  case '\u0006':  case '\u0007':
+			case '\u0008':  case '\t':  case '\n':  case '\u000b':
+			case '\u000c':  case '\r':  case '\u000e':  case '\u000f':
+			case '\u0010':  case '\u0011':  case '\u0012':  case '\u0013':
+			case '\u0014':  case '\u0015':  case '\u0016':  case '\u0017':
+			case '\u0018':  case '\u0019':  case '\u001a':  case '\u001b':
+			case '\u001c':  case '\u001d':  case '\u001e':  case '\u001f':
+			case ' ':  case '!':  case '#':  case '$':
+			case '%':  case '&':  case '\'':  case '(':
+			case ')':  case '*':  case '+':  case ',':
+			case '-':  case '.':  case '/':  case '0':
+			case '1':  case '2':  case '3':  case '4':
+			case '5':  case '6':  case '7':  case '8':
+			case '9':  case ':':  case ';':  case '<':
+			case '=':  case '>':  case '?':  case '@':
+			case 'A':  case 'B':  case 'C':  case 'D':
+			case 'E':  case 'F':  case 'G':  case 'H':
+			case 'I':  case 'J':  case 'K':  case 'L':
+			case 'M':  case 'N':  case 'O':  case 'P':
+			case 'Q':  case 'R':  case 'S':  case 'T':
+			case 'U':  case 'V':  case 'W':  case 'X':
+			case 'Y':  case 'Z':  case '[':  case ']':
+			case '^':  case '_':  case '`':  case 'a':
+			case 'b':  case 'c':  case 'd':  case 'e':
+			case 'f':  case 'g':  case 'h':  case 'i':
+			case 'j':  case 'k':  case 'l':  case 'm':
+			case 'n':  case 'o':  case 'p':  case 'q':
+			case 'r':  case 's':  case 't':  case 'u':
+			case 'v':  case 'w':  case 'x':  case 'y':
+			case 'z':  case '{':  case '|':  case '}':
+			case '~':  case '\u007f':
+			{
+				matchNot('"');
+				break;
+			}
+			default:
+			{
+				break _loop15;
+			}
+			}
+		} while (true);
+		}
+		match('"');
+		if ( _createToken && _token==null && _ttype!=Token.SKIP ) {
+			_token = makeToken(_ttype);
+			_token.setText(new String(text.getBuffer(), _begin, text.length()-_begin));
+		}
+		_returnToken = _token;
+	}
+	
+	
+	private static final long[] mk_tokenSet_0() {
+		long[] data = { -1025L, -1L, 0L, 0L};
+		return data;
+	}
+	public static final BitSet _tokenSet_0 = new BitSet(mk_tokenSet_0());
+	
+	}
---
+++ java/decaf/DecafScannerTokenTypes.java
@@ -0,0 +1,15 @@
+// $ANTLR 2.7.7 (2006-11-01): "Lexer.g" -> "DecafScanner.java"$
+package decaf;
+public interface DecafScannerTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int TK_class = 4;
+	int LCURLY = 5;
+	int RCURLY = 6;
+	int ID = 7;
+	int WS_ = 8;
+	int SL_COMMENT = 9;
+	int CHAR = 10;
+	int STRING = 11;
+	int ESC = 12;
+}
